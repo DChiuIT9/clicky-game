aa20bf36b2221230815e9c919a956965bd0adccd
Daniel Chiu <cycdaniel@gmail.com>
2019-11-01 02:17:05 -0500

    Initial file setup

---
+++ clicky-game/src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="img-container" onClick={() => props.removeFriend(props.id)}>
+        <img alt={props.name} src={props.image} />
+      </div>
+
+      {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span> */}
+    </div>
+  );
+}
+
+export default Card;
